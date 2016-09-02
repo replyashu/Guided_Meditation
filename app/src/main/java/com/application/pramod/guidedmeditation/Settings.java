c931a1f8660e5690411060cad2d4da7d1a8f8d9d
pramod <pramod0302@gmail.com>
2016-09-02 23:44:24 +0530

    Added back button in settings menu

--- app/src/main/java/com/application/pramod/guidedmeditation/Settings.java
+++ app/src/main/java/com/application/pramod/guidedmeditation/Settings.java
@@ -1,17 +1,32 @@
 package com.application.pramod.guidedmeditation;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 
 /**
  * Created by pramod on 02-09-2016.
  */
 public class Settings extends AppCompatActivity {
+    ActionBar mActionBar;
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.settings_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
